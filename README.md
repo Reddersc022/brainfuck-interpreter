c10234f8035ac79862efed0b1a10cf4a69c18b59
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2021-09-17 22:25:07 +0100

    Version 1

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# brainfuck-interpreter
- 
+# Brainfuck Interpreter  
+Enclosed is an interpreter, written in Python currently, to run [BrainFuck](https://en.wikipedia.org/wiki/Brainfuck) code.
+
+### Syntax
+- `>` : Increment pointer
+- `<` : Decrement pointer
+- `+` : Increment data at pointer
+- `-` : Decrement data at pointer
+- `.` : Output integer at pointer
+- `,` : Save one integer of input to pointer
+- `[` : If integer at pointer is 0, jump to after matching `]`, continue otherwise
+- `]` : If integer at pointer is non-0, jump to starting `[`, continue otherwise
+- `#` : Comment, anything afterward will be ignored (until newline)
+
+### Rules
+- All values stored are initially 0, and can only be integers.  
+- The array of values has a constant length (usually 30,000) and this can be edited in the code. Any attempt to access past the max length will result in an error.  
+- Any inputs must be either an integer or single character (not newline), anything otherwise causes an error
+
+### Usage
+``python src\brainfuck.py code_file``
+
+### Tests
+To add two inputted numbers:  
+``python src\brainfuck.py tests\add_two_numbers``  
+To echo any input:  
+``python src\brainfuck.py tests\echo``  
+Hello world!  
+``python src\brainfuck.py tests\hello_world``  
+
+### Notes
+- The end state will be saved as: ``outputs\end_states\dd-mm-yy hhmmss.json``
+- Any output will be saved as: ``outputs\output\dd-mm-yy hhmmss.txt``
+
+### TODO
+- [x] Implement basic syntax  
+- [x] Implement comments  
+- [ ] Update to C  
---
+++ src/brainfuck.py
@@ -0,0 +1,131 @@
+"""Main runfile for interpreting BrianFuck code"""
+
+import json
+import time
+from typing import List
+
+ARRAY_SIZE = 30_000
+
+
+def sort_file(file_name: str) -> str:
+    with open(file_name, "r") as f:
+        content = f.read().replace(" ", "").split("\n")
+    
+    code = ""
+    for line in content:
+        for char in line:
+            if char == "#":
+                break
+            else:
+                code += char
+    
+    return code
+
+
+def save_end_state(state: List[int], dir: str = "outputs\end_states"):
+    date_time = time.strftime("%d-%m-%y %H%M%S")
+    file_name = f"{dir}\\{date_time}.json"
+    with open(file_name, "w") as f:
+        json.dump(state, f)
+    print(f"End state saved at {file_name}")
+
+
+def add_to_output(value: int, file: str):
+    with open(file, "a") as f:
+        f.write(chr(value))
+
+
+def sort_error(ins_pointer: int, message: str):
+    print(f"** Error at character {ins_pointer}:")
+    print(message)
+
+
+def main(args: List[str]):
+    file = args[1]
+    code = sort_file(file)
+
+    array = [0 for _ in range(ARRAY_SIZE)]
+    array_pointer = 0
+
+    instruction_pointer = 0
+
+    in_loop = False
+    loop_stack = []
+
+    out_file = f"outputs\output\{time.strftime('%d-%m-%y %H%M%S')}.txt"
+
+    while instruction_pointer < len(code):
+        instruction = code[instruction_pointer]
+
+        # Sort instruction
+        if instruction == ">":
+            if array_pointer < 299:
+                array_pointer += 1
+            else:
+                sort_error(
+                    instruction_pointer,
+                    f"Array pointer should not be greater than array length ({ARRAY_SIZE})"
+                )
+                break
+        elif instruction == "<":
+            if array_pointer > 0:
+                array_pointer -= 1
+            else:
+                sort_error(
+                    instruction_pointer,
+                    "Array pointer should not be less than 0"
+                )
+                break
+        elif instruction == "+":
+            array[array_pointer] += 1
+        elif instruction == "-":
+            array[array_pointer] -= 1
+        elif instruction == ".":
+            print(f"Output: {chr(array[array_pointer])} (int: {array[array_pointer]})")
+            add_to_output(array[array_pointer], out_file)
+        elif instruction == ",":
+            inp = input("Input: ")
+            if inp.isdigit():
+                array[array_pointer] = int(inp)
+            elif len(inp) != 1:
+                sort_error(
+                    instruction_pointer,
+                    f"Input should be either an integer or single character (not {inp})"
+                )
+                break
+            else:
+                array[array_pointer] = ord(inp)
+        elif instruction == "[":
+            if array[array_pointer] == 0:
+                depth = 0
+                while depth >= 0:
+                    instruction_pointer += 1
+                    instruction = code[instruction_pointer]
+                    if instruction == "[":
+                        depth += 1
+                    elif instruction == "]":
+                        depth -= 1
+            else:
+                in_loop = True
+                loop_stack.append(instruction_pointer)
+        elif instruction == "]":
+            if not in_loop:
+                sort_error(
+                    instruction_pointer,
+                    f"`]` found when not in loop (no matching `[`)"
+                )
+                break
+            elif array[array_pointer] == 0:
+                loop_stack.pop(-1)
+            else:
+                instruction_pointer = loop_stack[-1]
+
+        # Finish up
+        instruction_pointer += 1
+    save_end_state(array)
+
+
+if __name__ == "__main__":
+    import sys
+    args = sys.argv
+    main(args)
